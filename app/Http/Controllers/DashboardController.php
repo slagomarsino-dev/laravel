0ad55105c6bdc204efa29602cc90df75f5019699
Sebastian Lagomarsino <sebastian.lagomarsino@lightit.io>
2023-02-09 09:55:30 -0300

    Add query param to dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,10 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-
         return view("dashboard", [
-            "title" => "Laravel Training"
+            "title" => $request->query("title", "Laravel Training")
         ]);
     }
 }
