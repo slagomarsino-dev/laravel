44e827e6c673c73cb3ad9b0de064fb315b4fee71
Sebastian Lagomarsino <sebastian.lagomarsino@lightit.io>
2023-02-09 09:49:02 -0300

    Add Home and Dashboard controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, "index"]);
 
-Route::get("/test", function () {
-    return view("test", [
-        "title" => "Laravel Training"
-    ]);
-});
+Route::get("/dashboard", [DashboardController::class, "index"]);
