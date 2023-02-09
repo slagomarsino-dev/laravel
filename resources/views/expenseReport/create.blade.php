0f8790c10936ade20634ed28adf6da52fb1dbc52
Sebastian Lagomarsino <sebastian.lagomarsino@lightit.io>
2023-02-09 11:57:09 -0300

    Add create expense reports route

---
+++ resources/views/expenseReport/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="row text-end">
+        <div class="col">
+            <h1>New Report</h1>
+
+            <div class="row">
+                <div class="col">
+                    <a class="btn btn-secondary" href="/expense_reports">Back</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <form action="/expense_reports" method="POST">
+                        <div class="form-group">
+                            <label for="title">:Title</label>
+                            <input type="text" class="form-control" id="title" name="title" />
+                        </div>
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
